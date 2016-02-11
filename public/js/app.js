36ba0fa8156097200b270c1849d24e36eca160cf
AndraIshmael <ishmaelak@gmail.com>
2016-02-10 20:38:46 -0500

    mostly functioning calculator

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   var calculator = Object.create(Calculator);
   var $display = $('#display');
   var lastOperation = '';
-
+  var num ='';
 
   $('#clear').on('click', clear);
   $('#plus').on('click', plus);
@@ -11,54 +11,110 @@ $(document).ready(function(){
   $('#multiply').on('click', multiply);
   $('#divide').on('click', divide);
   $('.num').on('click', updateDisplay);
-  $('mem-plus').on('click', memAdd);
-  $('mem-minus').on('click', memSub);
-  $('mem-recall').on('click', memDisplay);
-  $('mem-clear').on('click', memClear);
-
+  $('#mem-plus').on('click', memAdd);
+  $('#mem-minus').on('click', memSub);
+  $('#mem-recall').on('click', memDisplay);
+  $('#mem-clear').on('click', memClear);
+  $('#decimal').on('click', updateDisplay);
 
 
   function updateDisplay(){
-    
+     num += $(this).text();
+    $display.text(num);
+
   }
   function divide(){
-    
+      calculator.current = num;
+      num = '' ;
+
+
+      lastOperation = '/';
+
+
   }
 
   function multiply(){
-    
+    calculator.current = num;
+    num = '';
+
+
+    lastOperation='*';
+
   }
 
   function subtract(){
-    
+
+    calculator.current = num;
+    num = '';
+
+
+     lastOperation = '-';
   }
 
   function equal(){
-    
+
+    if (lastOperation == '+'){
+        calculator.add(num);
+        $display.text(calculator.current);
+    }
+    else if (lastOperation == '-') {
+        calculator.subtract(num);
+        $display.text(calculator.current);
+    }
+    else if (lastOperation == '/') {
+        calculator.divide(num);
+        $display.text(calculator.current);
+
+    }
+    else if(lastOperation == '*'){
+         calculator.multiply(num);
+         $display.text(calculator.current);
+
+    }
+    num = '';
+
   }
 
   function plus(){
-    
+
+    calculator.current = num;
+    num = '';
+
+
+    lastOperation = '+';
+    console.log(calculator.current);
+    console.log(lastOperation);
+
+
   }
 
   function clear(){
-    
+    num= '';
+    $display.text(num);
+
   }
 
   function memAdd(){
+     calculator.addToMemory();
 
   }
 
   function memSub(){
+     calculator.subtractFromMemory;
 
   }
 
   function memClear(){
+    calculator.clearMemory;
 
   }
 
   function memDisplay(){
-    
+    getMemory();
+   console.log(calculator.memory);
+    var mem = calculator.memory;
+    $display.text(mem);
+
   }
 
 
--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,4 +1,5 @@
 var Calculator = {
+
   current: 0,
   memory: 0,
   add: add,
@@ -9,22 +10,45 @@ var Calculator = {
   reset: reset,
   addToMemory: addToMemory,
   subtractFromMemory: subtractFromMemory,
+  displayMemory : getMemory,
 }
 
 function add(num){
+  num = parseInt(num);
+  this.current = parseInt(this.current);
   this.current += num;
+  console.log(this.current)
+  result();
 }
 function subtract(num){
-  
+  num = parseInt(num);
+  this.current = parseInt(this.current);
+  this.current -= num;
+
+
+
 }
 function multiply(num){
-  
+  num = parseInt(num);
+  this.current = parseInt(this.current);
+  this.current *= num;
+
+  result();
+  console.log(this.current);
+
 }
 function divide(num){
-  
+  num = parseInt(num);
+  this.current = parseInt(this.current);
+  this.current /= num;
+  result();
+  console.log(this.current);
+
 }
 function result(){
+
   return this.current;
+
 }
 function reset(num){
   this.current = 0;
@@ -32,6 +56,7 @@ function reset(num){
 
 function addToMemory(num){
   this.memory += num;
+  console.log('commited to memory')
 }
 
 function subtractFromMemory(num){
@@ -43,5 +68,7 @@ function clearMemory(){
 }
 
 function getMemory(){
+
   return this.memory;
+  console.log(this.memory);
 }
